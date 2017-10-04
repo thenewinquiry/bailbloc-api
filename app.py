4e84982f0f71234547f4a439994fa6fbaf5b3d3d
Francis Tseng <f+accounts@frnsys.com>
2017-10-04 10:34:34 -0400

    start scheduler

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from flask_apscheduler import APScheduler
 from collections import deque
 
 CACHE_SIZE = 200
-POLL_INTERVAL_MINS = 5
+POLL_INTERVAL_MINS = 1
 cache = deque(list(reversed(stats.last_n(CACHE_SIZE))), maxlen=CACHE_SIZE)
 app = Flask(__name__)
 
 
 def update_stats():
     """snapshot stats and append to cache"""
+    print('Updating stats')
     data = stats.snapshot_stats()
     cache.appendleft(data)
 
@@ -21,9 +22,12 @@ def history():
     most recent first"""
     return jsonify(list(cache))
 
+
 scheduler = APScheduler()
 scheduler.init_app(app)
 scheduler.add_job('update_stats', update_stats, **{'trigger': 'interval', 'minutes': POLL_INTERVAL_MINS})
+scheduler.start()
+
 
 if __name__ == '__main__':
     app.run()
