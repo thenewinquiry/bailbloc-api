67fc560f6d5efef396e9296e780090716d7ec9e1
Francis Tseng <f+accounts@frnsys.com>
2017-11-08 16:13:54 -0500

    added filelock

--- stats.py
+++ stats.py
@@ -1,6 +1,7 @@
 import os
 import mmap
 import json
+import filelock
 import requests
 from datetime import datetime
 
@@ -9,6 +10,7 @@ WALLET_ADDR = '442uGwAdS8c3mS46h6b7KMPQiJcdqmLjjbuetpCfSKzcgv4S56ASPdvXdySiMizGT
 TICKER_URL = 'https://api.cryptonator.com/api/ticker/xmr-usd'
 STATS_URL = 'https://api.xmrpool.net/miner/{}/stats'.format(WALLET_ADDR)
 MINER_URL = 'https://api.xmrpool.net/miner/{}/identifiers'.format(WALLET_ADDR)
+LOCK = filelock.FileLock('/tmp/stats.lock')
 
 
 def get_stats():
@@ -21,11 +23,12 @@ def get_stats():
 
 
 def snapshot_stats():
-    stats = get_stats()
-    with open(DUMP_FILE, 'a') as f:
-        f.write(json.dumps(stats))
-        f.write('\n')
-    return stats
+    with LOCK.acquire(timeout=10):
+        stats = get_stats()
+        with open(DUMP_FILE, 'a') as f:
+            f.write(json.dumps(stats))
+            f.write('\n')
+        return stats
 
 
 def last_n(n, step_size=1):
@@ -38,21 +41,22 @@ def last_n(n, step_size=1):
 # <https://stackoverflow.com/a/6813975>
 def tail(n):
     """returns last n lines from the dump"""
-    try:
-        size = os.path.getsize(DUMP_FILE)
-    except FileNotFoundError:
-        return []
-    if size == 0:
-        return []
-    with open(DUMP_FILE, 'rb') as f:
-        fm = mmap.mmap(f.fileno(), 0, mmap.MAP_SHARED, mmap.PROT_READ)
+    with LOCK.acquire(timeout=5):
         try:
-            for i in range(size - 1, -1, -1):
-                if fm[i] == ord('\n'):
-                    n -= 1
-                    if n == -1:
-                        break
-            return fm[i + 1 if i else 0:].splitlines()
-        finally:
-            fm.close()
+            size = os.path.getsize(DUMP_FILE)
+        except FileNotFoundError:
+            return []
+        if size == 0:
+            return []
+        with open(DUMP_FILE, 'rb') as f:
+            fm = mmap.mmap(f.fileno(), 0, mmap.MAP_SHARED, mmap.PROT_READ)
+            try:
+                for i in range(size - 1, -1, -1):
+                    if fm[i] == ord('\n'):
+                        n -= 1
+                        if n == -1:
+                            break
+                return fm[i + 1 if i else 0:].splitlines()
+            finally:
+                fm.close()
 
